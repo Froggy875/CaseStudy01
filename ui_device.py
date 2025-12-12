af5d2c549a5f23251a9d536ad7cb129dd3ec05df
Mathias Ausserlechner <mathias.ausserlechner@gmail.com>
2025-12-12 12:17:18 +0100

    User-Login_Eingabe

--- ui_device.py
+++ ui_device.py
@@ -1,39 +1,59 @@
 import streamlit as st
 from datetime import datetime, time
 
-# Eine Überschrift der ersten Ebene
-st.write("# Gerätemanagement")
+tab1, tab2 = st.tabs(["User-Login", "Gerätemanagement"])
 
-# Eine Überschrift der zweiten Ebene
-st.write("## Geräteauswahl")
+with tab2:
+   # Eine Überschrift der ersten Ebene
+   st.write("# Gerätemanagement")
 
-# Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
+   # Eine Überschrift der zweiten Ebene
+   st.write("## Geräteauswahl")
 
-current_device = st.selectbox(label='Gerät auswählen',
-        options = ["Gerät_A", "Gerät_B"])
+   # Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
 
-st.write(f"Das ausgewählte Gerät ist {current_device}")
+   current_device = st.selectbox(label='Gerät auswählen',
+         options = ["Gerät_A", "Gerät_B"])
 
-#Zeitauswahl
+   st.write(f"Das ausgewählte Gerät ist {current_device}")
 
-# 1) Datum wählen
-datum = st.date_input("Datum auswählen")
+   #Zeitauswahl
 
-# 2) Start- und Endzeit wählen
-col1, col2 = st.columns(2)
-with col1:
-    start_time = st.time_input("Startzeit", value=time(9, 0))
-with col2:
-    end_time = st.time_input("Endzeit", value=time(17, 0))
+   # 1) Datum wählen
+   datum = st.date_input("Datum auswählen")
 
-# 3) Kombinierten Zeitslot anzeigen
-if start_time >= end_time:
-    st.error("Endzeit muss nach der Startzeit liegen.")
-else:
-    start_dt = datetime.combine(datum, start_time)
-    end_dt = datetime.combine(datum, end_time)
-    st.write("Gewählter Zeitslot:")
-    st.write(start_dt, "–", end_dt)
+   # 2) Start- und Endzeit wählen
+   col1, col2 = st.columns(2)
+   with col1:
+      start_time = st.time_input("Startzeit", value=time(9, 0))
+   with col2:
+      end_time = st.time_input("Endzeit", value=time(17, 0))
+
+   # 3) Kombinierten Zeitslot anzeigen
+   if start_time >= end_time:
+      st.error("Endzeit muss nach der Startzeit liegen.")
+   else:
+      start_dt = datetime.combine(datum, start_time)
+      end_dt = datetime.combine(datum, end_time)
+      st.write("Gewählter Zeitslot:")
+      st.write(start_dt, "–", end_dt)
+
+with tab1:
+   st.write("# User-Login")
+   if st.button("New User"):
+      email = st.text_input(
+      label='Geben Sie hier Ihre Email-Adresse ein:',
+      value='' # Optionaler Startwert
+      )
+
+      pw = st.text_input(
+      label='Geben Sie hier Ihr Passwort ein:',
+      value='' # Optionaler Startwert
+      )
+      
+
+   login_button = st.button("login")
+
 
 
 
