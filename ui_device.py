7fcb28b3125ff9416b3def68088baf634e65b3ca
Mathias Ausserlechner <mathias.ausserlechner@gmail.com>
2025-12-17 15:50:31 +0100

    Included code from task slides

--- ui_device.py
+++ ui_device.py
@@ -1,58 +1,33 @@
 import streamlit as st
 from datetime import datetime, time
 
-tab1, tab2 = st.tabs(["User-Login", "Gerätemanagement"])
+# Eine Auswahlbox mit Datenbankabfrage, das Ergebnis wird in current_device gespeichert
+devices_in_db = find_devices()
 
-with tab2:
-   # Eine Überschrift der ersten Ebene
-   st.write("# Gerätemanagement")
+if devices_in_db:
+    current_device_name = st.selectbox(
+        'Gerät auswählen',
+        options=devices_in_db, key="sbDevice")
 
-   # Eine Überschrift der zweiten Ebene
-   st.write("## Geräteauswahl")
+    if current_device_name in devices_in_db:
+        loaded_device = Device.find_by_attribute("device_name", current_device_name)
+        if loaded_device:
+            st.write(f"Loaded Device: {loaded_device}") # nutzt __str__ Methode
+        else:
+            st.error("Device not found in the database.")
 
-   # Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
+        with st.form("Device"):
+            st.write(loaded_device.device_name) # Direkter Zugriff auf die Attribute
 
-   current_device = st.selectbox(label='Gerät auswählen',
-         options = ["Gerät_A", "Gerät_B"])
+            text_input_val = st.text_input("Geräte-Verantwortlicher", value=loaded_device.managed_by_user_id)
+            loaded_device.set_managed_by_user_id(text_input_val) # Nutzt die Setter-Methode
 
-   st.write(f"Das ausgewählte Gerät ist {current_device}")
-
-   #Zeitauswahl
-
-   # 1) Datum wählen
-   datum = st.date_input("Datum auswählen")
-
-   # 2) Start- und Endzeit wählen
-   col1, col2 = st.columns(2)
-   with col1:
-      start_time = st.time_input("Startzeit", value=time(9, 0))
-   with col2:
-      end_time = st.time_input("Endzeit", value=time(17, 0))
-
-   # 3) Kombinierten Zeitslot anzeigen
-   if start_time >= end_time:
-      st.error("Endzeit muss nach der Startzeit liegen.")
-   else:
-      start_dt = datetime.combine(datum, start_time)
-      end_dt = datetime.combine(datum, end_time)
-      st.write("Gewählter Zeitslot:")
-      st.write(start_dt, "–", end_dt)
-
-with tab1:
-   st.write("# User-Login")
-   if st.button("New User"):
-      email = st.text_input(
-      label='Geben Sie hier Ihre Email-Adresse ein:',
-      value='' # Optionaler Startwert
-      )
-
-      pw = st.text_input(
-      label='Geben Sie hier Ihr Passwort ein:',
-      value='' # Optionaler Startwert
-      )
-      
-
-   login_button = st.button("login")
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Submit")
+            if submitted:
+                loaded_device.store_data()
+                st.write("Data stored.")
+                st.rerun()
 
 
 
